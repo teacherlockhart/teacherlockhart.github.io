ff467f6b501d876f5c57890b5d28d992bcdb8c4f
Scott Lockhart <scott.lockhart@gmail.com>
2016-08-05 16:50:17 +0000

    updated portfolio

---
+++ my_website/files/flatulence_fan.ino
@@ -0,0 +1,41 @@
+
+int pushbutton = 6;
+int fan = 7;
+int buzzer = 8;
+int greenlight = 11;
+int yellowlight = 9;
+int redlight = 10;
+
+
+
+void setup() {
+ 
+  // put your setup code here, to run once:
+pinMode(pushbutton, INPUT_PULLUP);
+pinMode(fan, OUTPUT);
+pinMode(buzzer, OUTPUT);
+pinMode(greenlight, OUTPUT);
+pinMode(yellowlight, OUTPUT);
+pinMode(redlight, OUTPUT);
+
+}
+
+void loop() {
+  
+  // put your main code here, to run repeatedly:
+  if (digitalRead(pushbutton)==LOW)
+  {
+    digitalWrite(fan, HIGH);
+    digitalWrite(redlight, HIGH);
+    delay (5000);
+    digitalWrite(redlight, LOW);
+    digitalWrite (yellowlight, HIGH);
+    delay (5000);
+    digitalWrite(yellowlight, LOW);
+    digitalWrite(buzzer, HIGH);
+    digitalWrite(greenlight, HIGH);
+    delay (2000);
+    digitalWrite (greenlight, LOW);
+    digitalWrite (fan, LOW);
+  }
+}
